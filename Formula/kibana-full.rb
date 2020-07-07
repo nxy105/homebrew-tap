d509c67de7568990b6dac3c0b1fef5c750152ed5
Joshua <nxy105@qq.com>
2020-07-07 18:23:20 +0800

    Update kibana-full.rb

--- Formula/kibana-full.rb
+++ Formula/kibana-full.rb
@@ -1,7 +1,7 @@
 class KibanaFull < Formula
   desc "Analytics and search dashboard for Elasticsearch"
   homepage "https://www.elastic.co/products/kibana"
-  url "https://artifacts.elastic.co/downloads/kibana/kibana-7.8.0-darwin-x86_64.tar.gz?tap=elastic/homebrew-tap"
+  url "https://mirrors.huaweicloud.com/kibana/7.8.0/kibana-7.8.0-darwin-x86_64.tar.gz"
   version "7.8.0"
   sha256 "ff26d130926a4bc7c8ec344c27e51ad67e13dd27a073ddc63ea541f000981933"
   conflicts_with "kibana"
